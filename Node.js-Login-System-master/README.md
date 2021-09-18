72d35c5c946ac0f2333abd92a46537310a5fe10f
nsgoudarzi <69822293+nsgoudarzi1@users.noreply.github.com>
2021-09-18 18:58:47 -0500

    1

--- Node.js-Login-System-master/README.md
+++
@@ -1,5 +0,0 @@
-# Node.js Login System
-Basic Login System using Node.js, Express.js, and MySQL
-
-# Credit to Tutorials from:
-https://codeshack.io/basic-login-system-nodejs-express-mysql/
---
+++ node_admin_template_integration-main/controllers/homeController.js
@@ -0,0 +1,37 @@
+'use strict';
+const { LocalStorage } = require('node-localstorage');
+const localstorage = new LocalStorage('./scracth');
+
+const indexView = (req, res, next) => {
+    const data = localstorage.getItem('user') !== null ? JSON.parse(localstorage.getItem('user')) : null;
+    if (data !== null) {
+        res.render('home');
+    } else {
+        res.redirect('/login');
+    }
+}
+
+const iconsView = (req, res, next) => {
+    res.render('icons');
+}
+
+const mapView = (req, res, next) => {
+    res.render('map');
+}
+
+const profileView = (req, res, next) => {
+    res.render('profile');
+}
+
+const tableView = (req, res, next) => {
+    res.render('table');
+}
+
+
+module.exports = {
+    indexView,
+    iconsView,
+    mapView,
+    profileView,
+    tableView
+}
